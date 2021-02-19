6bf979bff395b33b815a782bcd1c4057cbf97da9
nadiia <nadiia.oleks@gmail.com>
2021-02-19 15:09:15 +0200

    created multidimensional array with questions and answers

---
+++ js/quiz.js
@@ -0,0 +1,13 @@
+// 1. Create a multidimensional array to hold quiz questions and answers
+const questions = [
+    ["Which planet is closest to the sun?", "Mercury"],
+    ["How many players are there in a football team?", "11"],
+    ["What planet do you live on?", "Earth"],
+    ["An emu can fly (choose: true/false)", "false"],
+    ["What color is the sky?", "blue"],
+    ["A woman has walked on the Moon (choose: true/false)", "false"],
+    ["How many days does it take for the Earth to orbit the Sun?", "365"],
+    ["What’s the capital of Canada?", "Ottawa"],
+    ["When was  the World Wide Web invented?", "1990"],
+    ["What city do The Beatles come from?", "Liverpool"]
+];
