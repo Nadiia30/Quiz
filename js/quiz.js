eb6b6abc4ac7a60627e36191835dd02b82468405
nadiia <nadiia.oleks@gmail.com>
2021-02-19 15:32:05 +0200

    created function to list questions

--- js/quiz.js
+++ js/quiz.js
@@ -39,3 +39,12 @@ for (let i=0; i < questions.length; i++){
         incorrect.push(question);
     }
 }
+
+//4. Create list of questions
+function createListItems(arr){
+    let items = "";
+    for (let i=0; i < arr.length; i++){
+        items+= `<li>${arr[i]}</li>`;
+    }
+    return items;
+}
